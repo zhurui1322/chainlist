424137ba5560a9ed03b38c1dfb33cbc56e8ca128
Raymond Zhu <ruzhu@syr.edu>
2018-01-19 21:57:55 -0500

    Adding Frontend files to Github Page

--- test/ChainListExceptions.js
+++ test/ChainListExceptions.js
@@ -11,17 +11,18 @@ contract('ChainList', function(accounts) {
   var articleDescription = "Description for article 1";
   var articlePrice = 10;
 
+
   // Test case: buying an article when no article for sale yet
   it("should throw an exception if you try to buy an article when there is no article for sale", function() {
     return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle(articleId, {
-          from: buyer,
-          value: web3.toWei(articlePrice, "ether")
-        });
-      }).then(assert.fail)
+      chainListInstance = instance;
+      return chainListInstance.buyArticle(articleId, {
+        from: buyer,
+        value: web3.toWei(articlePrice, "ether")
+      });
+    }).then(assert.fail)
       .catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+        assert(error.message.indexOf('revert') >= 0, "error should be revert");
       }).then(function() {
         return chainListInstance.getNumberOfArticles();
       }).then(function(data) {
@@ -33,18 +34,18 @@ contract('ChainList', function(accounts) {
   // Test case: buying an article that does not exist
   it("should throw an exception if you try to buy an article that does not exist", function() {
     return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {
-          from: seller
-        });
-      }).then(function(receipt) {
-        return chainListInstance.buyArticle(2, {
-          from: buyer,
-          value: web3.toWei(articlePrice, "ether")
-        });
-      }).then(assert.fail)
+      chainListInstance = instance;
+      return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {
+        from: seller
+      });
+    }).then(function(receipt) {
+      return chainListInstance.buyArticle(2, {
+        from: buyer,
+        value: web3.toWei(articlePrice, "ether")
+      });
+    }).then(assert.fail)
       .catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+        assert(error.message.indexOf('revert') >= 0, "error should be revert");
       }).then(function() {
         return chainListInstance.articles(articleId);
       }).then(function(data) {
@@ -60,14 +61,14 @@ contract('ChainList', function(accounts) {
   // Test case: buying an article you are selling
   it("should throw an exception if you try to buy your own article", function() {
     return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle(articleId, {
-          from: seller,
-          value: web3.toWei(articlePrice, "ether")
-        });
-      }).then(assert.fail)
+      chainListInstance = instance;
+      return chainListInstance.buyArticle(articleId, {
+        from: seller,
+        value: web3.toWei(articlePrice, "ether")
+      });
+    }).then(assert.fail)
       .catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+        assert(error.message.indexOf('revert') >= 0, "error should be revert");
       }).then(function() {
         return chainListInstance.articles(articleId);
       }).then(function(data) {
@@ -84,14 +85,14 @@ contract('ChainList', function(accounts) {
   // Test case: incorrect value
   it("should throw an exception if you try to buy an article for a value different from its price", function() {
     return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle(articleId, {
-          from: buyer,
-          value: web3.toWei(articlePrice + 1, "ether")
-        });
-      }).then(assert.fail)
+      chainListInstance = instance;
+      return chainListInstance.buyArticle(articleId, {
+        from: buyer,
+        value: web3.toWei(articlePrice + 1, "ether")
+      });
+    }).then(assert.fail)
       .catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+        assert(error.message.indexOf('revert') >= 0, "error should be revert");
       }).then(function() {
         return chainListInstance.articles(articleId);
       }).then(function(data) {
@@ -108,19 +109,19 @@ contract('ChainList', function(accounts) {
   // Test case: article has already been sold
   it("should throw an exception if you try to buy an article that has already been sold", function() {
     return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle(articleId, {
-          from: buyer,
-          value: web3.toWei(articlePrice, "ether")
-        });
-      }).then(function() {
-        return chainListInstance.buyArticle(articleId, {
-          from: web3.eth.accounts[0],
-          value: web3.toWei(articlePrice, "ether")
-        });
-      }).then(assert.fail)
+      chainListInstance = instance;
+      return chainListInstance.buyArticle(articleId, {
+        from: buyer,
+        value: web3.toWei(articlePrice, "ether")
+      });
+    }).then(function() {
+      return chainListInstance.buyArticle(articleId, {
+        from: web3.eth.accounts[0],
+        value: web3.toWei(articlePrice, "ether")
+      });
+    }).then(assert.fail)
       .catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+        assert(error.message.indexOf('revert') >= 0, "error should be revert");
       }).then(function() {
         return chainListInstance.articles(articleId);
       }).then(function(data) {
